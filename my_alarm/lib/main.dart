89a30e8f08118a0d66a5ba7801e56f40c6854f3d
ajvelo <>
2019-12-09 17:03:01 +0000

    Add Tabbarcontroller

--- my_alarm/lib/main.dart
+++ my_alarm/lib/main.dart
@@ -24,17 +24,69 @@ class AppClock extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      body: new Padding(
-        padding: const EdgeInsets.only(left: 10.0, right: 10.0),
-        child: new Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-
-            new Clock(),
-          ],
+    return new Container(
+      height: 600,
+      width: double.infinity,
+      child: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0.0,
+            backgroundColor: Colors.transparent,
+            bottom: PreferredSize(
+              preferredSize: Size.fromHeight(55),
+              child: Container(
+                color: Colors.transparent,
+                child: SafeArea(
+                  child: Column(
+                    children: <Widget>[
+                      TabBar(
+                        indicator: UnderlineTabIndicator(
+                          borderSide: BorderSide(
+                            color: Color(0xffff0863),
+                            width: 4.0
+                          ),
+                          insets: EdgeInsets.fromLTRB(40.0, 20.0, 40.0, 0)
+                        ),
+                        indicatorWeight: 15,
+                        labelColor: Color(0xff2d386b),
+                        labelStyle: TextStyle(
+                          fontSize: 12,
+                          letterSpacing: 1.3,
+                          fontWeight: FontWeight.w500
+                        ),
+                        tabs: <Widget>[
+                          Tab(
+                            text: 'ALARMS',
+                            icon: Icon(
+                              Icons.watch_later,
+                              size: 40,
+                            ),
+                          ),
+                          Tab(
+                            text: 'RECORDS',
+                            icon: Icon(
+                              Icons.menu,
+                              size: 40,
+                            ),
+                          ),
+                          Tab(
+                            text: 'PROFILE',
+                            icon: Icon(
+                              Icons.supervised_user_circle,
+                              size: 40,
+                            ),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
         ),
-      ),
+      )
     );
   }
 }
